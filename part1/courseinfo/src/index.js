1fb690ff18cd5dae8a671f8bb89bbb6e65bf1c20
Brent <bbenofsky@gmail.com>
2020-04-08 01:12:07 +0800

    finish exercise 1.2

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -14,19 +14,6 @@ const App = () => {
   const exercises3 = 14;
 
   return (
-    // <div>
-    //   <h1>{course}</h1>
-    //   <p>
-    //     {part1} {exercises1}
-    //   </p>
-    //   <p>
-    //     {part2} {exercises2}
-    //   </p>
-    //   <p>
-    //     {part3} {exercises3}
-    //   </p>
-    //   <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-    // </div>
     <div>
       <Header course={course} />
       <Content
