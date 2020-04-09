ef681306779b1be74abe2e9c14479b2f3bbb942c
Brent <bbenofsky@gmail.com>
2020-04-09 15:21:14 +0800

    finish exercise 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,9 +3,13 @@ import React, { useState } from 'react';
 function App() {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '040-1234567' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' },
   ]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const [filter, setFilter] = useState('');
 
   const handleNameChange = (e) => {
     setNewName(e.target.value);
@@ -28,9 +32,22 @@ function App() {
     }
   };
 
+  const display = persons.filter((person) =>
+    person.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with
+        <input
+          type='text'
+          value={filter}
+          onChange={(e) => setFilter(e.target.value)}
+        />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={handleSubmit}>
         <div>
           name:{' '}
@@ -45,7 +62,7 @@ function App() {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
+      {display.map((person) => (
         <div key={person.name}>
           {person.name} {person.number}
         </div>
